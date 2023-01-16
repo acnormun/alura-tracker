31344fae2423423a07b6dc37da607e067ebf639d
Ana Clara <anaclaranoronha.m@gmail.com>
2023-01-16 13:47:27 -0300

    setando as rotas

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,9 +1,27 @@
 <template>
     <header>
         <h1>
-            <img src="../assets/jojo.png" alt="Logo">
+            <img src="../assets/jojo.png" alt="Logo" class="logo">
         </h1>
-        <button class="button" @click="alterarTema">{{textoBotao}}</button>
+        <div class="has-text-certered">
+          <button class="button" @click="alterarTema">{{textoBotao}}</button>
+        </div>
+        <nav class="panel mt-5">
+          <ul>
+            <li>
+              <router-link to="/" class="link">
+                <i class="fas fa-tasks"></i>
+                Tarefas
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/projetos" class="link">
+                <i class="fas fa-project-diagram"></i>
+                Projetos
+              </router-link>
+            </li>
+          </ul>
+        </nav>
     </header>
 
 </template>
@@ -44,10 +62,28 @@ header {
   height: 100vh;
   text-align: center;
 }
+
+.logo{
+  width: 20rem;
+  border-radius: 10rem;
+  margin-bottom: 1.25rem;
+}
 @media only screen and (max-width: 768px) {
   header {
     padding: 2.5rem;
     height: auto;
   }
+}
+.panel li {
+    margin: 8px 0;
+}
+.link {
+    color: #fff;
+}
+.link:hover {
+    color: #FAF0CA;
+}
+.link.router-link-active {
+    color: #FAF0CA;
 }
 </style>

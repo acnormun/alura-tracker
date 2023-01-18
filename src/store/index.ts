3112ea06aea074df3a48972f6e11282ac26016d2
Ana Clara <anaclaranoronha.m@gmail.com>
2023-01-18 16:30:22 -0300

    filtrando tarefas

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,78 +1,46 @@
-import { projeto } from './modulos/projeto/index';
-import { OBTER_TAREFAS, CADASTRAR_TAREFA, ALTERAR_TAREFA } from "./tipo-acoes";
-import { INotificacao } from "@/interfaces/INotificacao";
-import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
-import {
-  NOTIFICAR,
-  DEFINIR_TAREFAS,
-  ADICIONA_TAREFAS,
-  ALTERA_TAREFA,
-} from "./tipo-mutacoes";
-import http from "@/http";
-import ITarefa from "@/interfaces/ITarefa";
-import {EstadoProjeto} from './modulos/projeto'
+import { InjectionKey } from 'vue'
+import { NOTIFICAR } from "./tipo-mutacoes";
+import { INotificacao } from "@/interfaces/INotificacao";
+
+import { EstadoProjeto, projeto } from "./modulos/projeto";
+import { EstadoTarefa, tarefa } from "./modulos/tarefa";
 
 export interface Estado {
-  projeto: EstadoProjeto,
-  tarefas: ITarefa[];
-  notificacoes: INotificacao[];
+    notificacoes: INotificacao[],
+    projeto: EstadoProjeto,
+    tarefa: EstadoTarefa
 }
 
-export const key: InjectionKey<Store<Estado>> = Symbol();
+export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
-  state: {
-    projeto: {
-      projetos: [],
+    state: {
+        notificacoes: [],
+        projeto: {
+            projetos: []
+        },
+        tarefa: {
+            tarefas: []
+        }
     },
-    tarefas: [],
-    notificacoes: [],
-  },
-  mutations: {
-    [DEFINIR_TAREFAS](state, tarefas: ITarefa[]) {
-      state.tarefas = tarefas;
-    },
-    [ADICIONA_TAREFAS](state, tarefa: ITarefa) {
-      state.tarefas.push(tarefa);
-    },
-    [ALTERA_TAREFA](state, tarefa: ITarefa) {
-      const index = state.tarefas.findIndex((t) => t.id == tarefa.id);
-      state.tarefas[index] = tarefa;
-    },
-    [NOTIFICAR](state, novaNotificacao: INotificacao) {
-      novaNotificacao.id = new Date().getTime();
-      state.notificacoes.push(novaNotificacao);
+    mutations: {
+        [NOTIFICAR] (state, novaNotificacao: INotificacao) {
 
-      setTimeout(() => {
-        state.notificacoes = state.notificacoes.filter(
-          (notificacao) => notificacao.id != novaNotificacao.id
-        );
-      }, 3000);
+            novaNotificacao.id = new Date().getTime()
+            state.notificacoes.push(novaNotificacao)
+
+            setTimeout(() => {
+                state.notificacoes = state.notificacoes.filter(notificacao => notificacao.id != novaNotificacao.id)
+            }, 3000)
+        }
     },
-  },
-  actions: {
-    [OBTER_TAREFAS]({ commit }) {
-      http
-        .get("/tarefas/")
-        .then((response) => commit(DEFINIR_TAREFAS, response.data));
-    },
-    [CADASTRAR_TAREFA]({ commit }, tarefa: ITarefa) {
-      return http
-        .post("/tarefas", tarefa)
-        .then((resposta) => commit(ADICIONA_TAREFAS, resposta.data));
-    },
-    [ALTERAR_TAREFA]({ commit }, tarefa: ITarefa) {
-      return http
-        .put(`/tarefas/${tarefa.id}`, tarefa)
-        .then((resposta) => commit(ALTERA_TAREFA, tarefa));
-    },
-  },
-  modules: {
-    projeto
-  }
-});
+    modules: {
+        projeto,
+        tarefa
+    }
+})
 
 export function useStore(): Store<Estado> {
-  return vuexUseStore(key);
+    return vuexUseStore(key)
 }
--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,14 +1,31 @@
 <template>
   <Formulario @aoSalvarTarefa="salvarTarefa" />
   <div class="lista">
+    <Box v-if="listaVazia"> Bora Trabalhar! </Box>
+    <div class="field">
+      <p class="control has-icons-left has-icons-right">
+        <input
+          class="input"
+          type="email"
+          placeholder="Buscar tarefa..."
+          v-model="filtro"
+        />
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>
+      </p>
+    </div>
     <Tarefa
       v-for="(tarefa, index) in tarefas"
       :key="index"
       :tarefa="tarefa"
       @aoTarefaClicada="selecionarTarefa"
     />
-    <Box v-if="listaVazia"> Bora Trabalhar! </Box>
-    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
+    <div
+      class="modal"
+      :class="{ 'is-active': tarefaSelecionada }"
+      v-if="tarefaSelecionada"
+    >
       <div class="modal-background"></div>
       <div class="modal-card">
         <header class="modal-card-head">
@@ -27,7 +44,9 @@
           </div>
         </section>
         <footer class="modal-card-foot">
-          <button class="button is-success" @click="alterarTarefa">Salvar alterações</button>
+          <button class="button is-success" @click="alterarTarefa">
+            Salvar alterações
+          </button>
           <button class="button" @click="fecharModal">Cancelar</button>
         </footer>
       </div>
@@ -36,13 +55,13 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
 import { useStore } from "@/store";
 import {
-ALTERAR_TAREFA,
+  ALTERAR_TAREFA,
   CADASTRAR_TAREFA,
   OBTER_PROJETOS,
   OBTER_TAREFAS,
@@ -76,18 +95,28 @@ export default defineComponent({
     fecharModal() {
       this.tarefaSelecionada = null;
     },
-    alterarTarefa(){
-      this.store.dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
-        .then(() => this.fecharModal())
-    }
+    alterarTarefa() {
+      this.store
+        .dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
+        .then(() => this.fecharModal());
+    },
   },
   setup() {
+    const filtro = ref("");
+    const tarefas = computed(() =>
+      store.state.tarefa.tarefas.filter(
+        (t) => !filtro.value || t.descricao.includes(filtro.value)
+      )
+    );
+
     const store = useStore();
     store.dispatch(OBTER_TAREFAS);
     store.dispatch(OBTER_PROJETOS);
+
     return {
-      tarefas: computed(() => store.state.tarefas),
+      tarefas,
       store,
+      filtro,
     };
   },
 });
